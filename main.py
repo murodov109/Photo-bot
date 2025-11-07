7c6da9d721940697429a747a1e4843a80fd6abf8
murodov109 <akabekov77@gmail.com>
2025-11-08 03:50:45 +0500

    Update main.py

--- main.py
+++ main.py
@@ -282,4 +282,3 @@ if __name__=='__main__':
     threading.Thread(target=cleaner,daemon=True).start()
     print('Bot started')
     bot.infinity_polling(skip_pending=True, timeout=20, long_polling_timeout=20)
-```0
